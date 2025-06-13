77e27279529ad4cdf3217978fc2a6d73813bda8b
delarissss <lugovskaaarina@yandex.ru>
2025-06-13 19:27:36 +0300

    #9 Добавление функции расчета стоимости

--- src/main/java/org/example/Payment.java
+++ src/main/java/org/example/Payment.java
@@ -3,4 +3,33 @@ package org.example;
 import java.util.Scanner;
 
 public class Payment {
+    // Rонстанты для способов оплаты
+    public static final String CARD = "Картой";
+    public static final String CASH = "Наличными";
+    public static final String SBP = "По СБП";
+
+    // Константы для коэффициентов
+    public static final double ECONOMY = 0.9; // 10% скидка за эконом доставку
+    public static final double EXPRESS = 1.1; // 10% наценка за экспресс-доставку
+    public static final double DOCUMENTS = 1.2; // 20% наценка за доставку документов
+    public static final double STANDARD = 1; // стандартный тип доставки
+    public static final double COMMISSION_CARD = 0.02;   // 2% комиссия за оплату картой
+    public static final double COMMISSION_SBP = 0.01; // 1% комиссия за оплату по CБП
+    public static final double VAT = 0.2; // 20% НДС
+    // Расчет стоимости доставки
+    public static double calculateCost(String deliveryType, double price, String paymentMethod) {
+        double commission = 0;
+        double discount = switch (deliveryType) {
+            case "ECONOMY" -> price * ECONOMY;
+            case "EXPRESS" -> price * EXPRESS;
+            case "DOCUMENTS" -> price * DOCUMENTS;
+            default -> price * STANDARD;
+        };
+        if (paymentMethod.equals(CARD)) {
+            commission = COMMISSION_CARD;
+        } else if (paymentMethod.equals(SBP)) {
+            commission = COMMISSION_SBP;
+        }
+        return discount * (1 + VAT) * (1+ commission);
+    }
 }
