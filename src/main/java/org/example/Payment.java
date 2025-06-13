63a78be69b3a92b65c04aff2aae1d4d1b50ecd71
delarissss <lugovskaaarina@yandex.ru>
2025-06-13 19:38:44 +0300

    #10 Добавление вывода чека

--- src/main/java/org/example/Payment.java
+++ src/main/java/org/example/Payment.java
@@ -55,4 +55,49 @@ public class Payment {
         total.append("Метод оплаты: ").append(paymentMethod).append("\n");
         return total.toString();
     }
+    static String paymentProcess(String deliveryType, double price) {
+        Scanner scanner = new Scanner(System.in);
+        String paymentMethod = null;
+        System.out.println("Выберите способ оплаты:");
+        System.out.println("1. Картой");
+        System.out.println("2. Наличными");
+        System.out.println("3. По СБП");
+        System.out.println("4. Выход");
+        String input = scanner.nextLine();
+        if ("4".equals(input)) {
+            return "Оплата отменена";
+        }
+        paymentMethod = switch (input) {
+            case "1" -> CARD;
+            case "2" -> CASH;
+            case "3" -> SBP;
+            default -> throw new IllegalStateException("Unexpected value: " + input);
+        };
+        calculateCost(deliveryType, price, paymentMethod);
+        getCheck(deliveryType, price, paymentMethod);
+        if (paymentMethod.equals(CARD)) {
+            System.out.println("Введите данные карты: номер, срок действия и код безопасности");
+            System.out.print("Номер карты: ");
+            String cardNumber = scanner.nextLine();
+            System.out.print("Срок действия: ");
+            String expiryDate = scanner.nextLine();
+            System.out.print("Код безопасности: ");
+            String cvc = scanner.nextLine();
+            addCard(cardNumber, expiryDate, cvc);
+        } else if (paymentMethod.equals(SBP)) {
+            System.out.println("Сканируйте QR-код");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return "Оплата завершена";
+    }
+    public static String addCard(String cardNumber, String expiryDate, String cvc) {
+        PaymentCard paymentCard = new PaymentCard(cardNumber, expiryDate, cvc);
+        if (paymentCard.isValid()) {
+            return "Карта добавлена.";
+        } else return "Неверный ввод данных карты.";
+    }
 }
